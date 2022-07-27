1fcfe9662a3e6a84d9e7703a1171cc31e5e367e7
tejuypatil <tejuypatil@gmail.com>
2022-07-27 10:03:38 +0530

    Test case to find the Maximum String

--- src/com/brigelab/test/TestMaximumUtil.java
+++ src/com/brigelab/test/TestMaximumUtil.java
@@ -43,4 +43,24 @@ public class TestMaximumUtil {
         Double maximum = MaximumUtil.maximumFloat( 1.2, 2.31,14.3);
         Assert.assertEquals((Double) 14.3, maximum);
     }
+    //------------------- Test for String--------------------------
+
+    @Test
+    public void givenStringFirstNumberIsMaxNumberThenReturnFirstNumberAsMax() {
+
+        String maxString = MaximumUtil.maximumString("apple","cat","ball");
+        Assert.assertEquals("apple", maxString);
+    }
+
+    @Test
+    public void givenStringSecondNumberIsMaXNumberThenReturnSecondNumberAsMaximum() {
+        String maxString = MaximumUtil.maximumString("cat","apple","ball");
+        Assert.assertEquals("apple", maxString);
+    }
+
+    @Test
+    public void givenStringThirdNumberIsMaXNumberThenReturnThirdNumberAsMaximum() {
+        String maxString = MaximumUtil.maximumString("cat","ball","apple");
+        Assert.assertEquals( "apple", maxString);
+    }
 }
